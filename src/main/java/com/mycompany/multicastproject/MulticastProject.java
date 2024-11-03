afe40a5228e8c5b9590ab68cc57575feaefc1552
Le Duc Thai <leducthai1008@gmail.com>
2024-11-03 13:01:10 +0700

    multicast

---
+++ src/main/java/com/mycompany/multicastproject/MulticastProject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multicastproject;
+
+/**
+ *
+ * @author acer
+ */
+public class MulticastProject {
+
+    public static String name;
+    
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+        
+    }
+}
